c4bab47780a797d7717c66d65c708cb776c8547f
doch13_ <dohui0922@dimigo.hs.kr>
2022-10-09 13:33:01 +0900

    feat: Server SourceCode

---
+++ README.md
@@ -0,0 +1,9 @@
+# 한글 MBTI Backend
+
+펀딩 시작 시 알림을 위해 정보를 수집하는 서비스입니다.
+
+Flask를 통해 제작하였으며, Docker Container로 만들어 CloudType에 배포하였습니다.
+
+## 개발을 위해선
+
+Firebase Dashboard에서 Admin Key 파일을 다운받아 'firebase-adminkey.json' 이름으로 프로젝트 경로 제일 상단에 파일을 저장하여야 합니다.
---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from datetime import datetime
+
+
+app = Flask(__name__)
+CORS(app)
+
+cred = credentials.Certificate('firebase-adminkey.json')
+firebase_admin.initialize_app(cred,{
+    'databaseURL' : 'https://hangeul-mbti-default-rtdb.asia-southeast1.firebasedatabase.app' 
+})
+
+
+
+@app.route("/")
+def hello():
+	return "본 API는 GET 형식 요청을 받지 않습니다."
+
+@app.route('/', methods=['POST'])
+def main():
+    nowTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    requestData = request.json
+    phoneNum = requestData['phoneNum']
+
+    ref = db.reference('phoneNumList')
+    ref.update({nowTime: phoneNum})
+
+    
+    return jsonify({"phoneNum": phoneNum}), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
